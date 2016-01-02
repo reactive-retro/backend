96b11ff00f51fce7b1e964fad7f824895be39a93
seiyria <kyle@seiyria.com>
2016-01-02 14:01:47 -0600

    binding to heroku port if specified

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var SocketCluster = require('socketcluster').SocketCluster;
 var socketCluster = new SocketCluster({
     workers: Number(argv.w) || 1,
     stores: Number(argv.s) || 1,
-    port: Number(argv.p) || 8080,
+    port: Number(argv.p) || process.env.PORT || 8080,
     appName: argv.n || 'reactive-retro',
     workerController: __dirname + '/worker.js',
     storeController: __dirname + '/store.js',
