d979a73f78affe126e8fb6cbaec168f759f8caef
seiyria <kyle@seiyria.com>
2016-01-04 08:22:25 -0600

    change path elsewhere too

--- socketcluster/worker.js
+++ socketcluster/worker.js
@@ -6,7 +6,7 @@ export var run = (worker) => {
 
     scServer.on('connection', (socket) => {
 
-        const normalizedPath = require('path').join(__dirname, 'src', 'functions', 'socket');
+        const normalizedPath = require('path').join(__dirname, '..', 'src', 'functions', 'socket');
 
         _.each(require('fs').readdirSync(normalizedPath), (file) => {
             require(`../src/functions/socket/${file}`)(socket);
