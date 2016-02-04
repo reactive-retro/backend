ce2344c579036be7c5be23e6c3a3c6390c5bef4b
seiyria <kyle@seiyria.com>
2016-02-04 14:27:33 -0600

    removing unneeded code

--- src/character/professions/Cleric.js
+++ src/character/professions/Cleric.js
@@ -10,10 +10,4 @@ import { hp, mp, str, dex, vit, mnt, luk } from '../../static/decorators';
 @mnt(2)
 @luk(0.5)
 export default class Cleric extends Profession {
-    static hp(player) { return this.getLevel(player) * 7; }
-    static mp(player) { return this.getLevel(player) * 3 + this.getStat(player, 'mnt'); }
-    static str(player) { return this.getLevel(player) * 2; }
-    static mnt(player) { return this.getLevel(player) * 2; }
-    static dex(player) { return this.getLevel(player) * 1; }
-    static vit(player) { return this.getLevel(player) * 1; }
 }
--- src/character/professions/Thief.js
+++ src/character/professions/Thief.js
@@ -10,10 +10,4 @@ import { hp, mp, str, dex, vit, mnt, luk } from '../../static/decorators';
 @mnt(1)
 @luk(0.75)
 export default class Thief extends Profession {
-    static hp(player) { return this.getLevel(player) * 7 + this.getStat(player, 'vit'); }
-    static mp(player) { return this.getLevel(player) * 3; }
-    static str(player) { return this.getLevel(player) * 2; }
-    static mnt(player) { return this.getLevel(player) * 2; }
-    static dex(player) { return this.getLevel(player) * 1; }
-    static vit(player) { return this.getLevel(player) * 1; }
 }
