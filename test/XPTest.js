dccab3d067e5915dddb697340335c70574220355
seiyria <kyle@seiyria.com>
2016-03-03 13:23:14 -0600

    fixing tests to properly factor in bonus xp

--- test/XPTest.js
+++ test/XPTest.js
@@ -12,11 +12,15 @@ test('XP needed for level 50 = 100000 XP', t => {
 });
 
 test('XP gained for killing monsters is correct', t => {
-    t.true(XPCalculator.givenXp({ rating: 0,  currentLevel: 1 }) === 13);
-    t.true(XPCalculator.givenXp({ rating: -2, currentLevel: 1 }) === 7);
-    t.true(XPCalculator.givenXp({ rating: 5,  currentLevel: 1 }) === 28);
+    t.true(XPCalculator.givenXp({ rating: 0,  bonusXp: 0, currentLevel: 1 }) === 13);
+    t.true(XPCalculator.givenXp({ rating: -2, bonusXp: 0, currentLevel: 1 }) === 7);
+    t.true(XPCalculator.givenXp({ rating: 5,  bonusXp: 0, currentLevel: 1 }) === 28);
 
-    t.true(XPCalculator.givenXp({ rating: 0,  currentLevel: 10 }) === 22);
-    t.true(XPCalculator.givenXp({ rating: -2, currentLevel: 10 }) === 16);
-    t.true(XPCalculator.givenXp({ rating: 5,  currentLevel: 10 }) === 37);
+    t.true(XPCalculator.givenXp({ rating: 0,  bonusXp: 0, currentLevel: 10 }) === 22);
+    t.true(XPCalculator.givenXp({ rating: -2, bonusXp: 0, currentLevel: 10 }) === 16);
+    t.true(XPCalculator.givenXp({ rating: 5,  bonusXp: 0, currentLevel: 10 }) === 37);
+
+    t.true(XPCalculator.givenXp({ rating: 0,  bonusXp: 5, currentLevel: 10 }) === 27);
+    t.true(XPCalculator.givenXp({ rating: -2, bonusXp: 5, currentLevel: 10 }) === 21);
+    t.true(XPCalculator.givenXp({ rating: 5,  bonusXp: 5, currentLevel: 10 }) === 42);
 });
