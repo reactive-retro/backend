9b68f06aa5808177f9202c33aa6d93833b6acb8c
seiyria <kyle@seiyria.com>
2016-03-14 14:31:59 -0500

    stat rebalancing, cleric class has normal stats

--- src/character/professions/Cleric.js
+++ src/character/professions/Cleric.js
@@ -2,11 +2,11 @@
 import Profession from '../base/Profession';
 import { hp, mp, str, dex, vit, mnt, luk } from '../../static/decorators';
 
-@hp(7)
-@mp(3, (prof, player) => prof.getStat(player, 'mnt'))
+@hp(7, (prof, player) => Math.floor(prof.getStat(player, 'vit')))
+@mp(4, (prof, player) => prof.getStat(player, 'mnt'))
 @str(2)
 @dex(1)
-@vit(1)
+@vit(1.5)
 @mnt(2)
 @luk(0.5)
 export default class Cleric extends Profession {
--- src/character/professions/Fighter.js
+++ src/character/professions/Fighter.js
@@ -5,7 +5,7 @@ import { hp, mp, str, dex, vit, mnt, luk } from '../../static/decorators';
 @hp(10, (prof, player) => prof.getStat(player, 'vit') * 1.5)
 @mp(1)
 @str(3)
-@dex(1)
+@dex(1.5)
 @vit(1)
 @mnt(2)
 @luk(0.5)
