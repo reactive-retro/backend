8d78681aa73acaee2c0b337d9e3422ad43dd6716
seiyria <kyle@seiyria.com>
2016-03-14 12:36:38 -0500

    adding tests for spell data

---
+++ test/SpellDataTest.js
@@ -0,0 +1,39 @@
+
+import test from 'ava';
+import Dice from 'dice.js';
+
+import { allSkills } from '../src/objects/skillmanager';
+
+const allProfessions = require('require-dir')('../src/character/professions');
+
+const player = { str: 10, dex: 10, vit: 10, mnt: 10, luk: 10, acc: 10 };
+
+test('All spells have valid data', t => {
+    allSkills.forEach(skill => {
+        
+        t.ok(skill.spellName);
+        if(skill.spellCost) t.true(skill.spellCost >= 0);
+        if(skill.spellCooldown) t.true(skill.spellCooldown >= 0);
+
+        const usedProfessions = Object.keys(skill.spellClasses);
+        t.true(usedProfessions.length > 0);
+        usedProfessions.forEach(prof => {
+            t.true(prof === 'All' || !!allProfessions[prof]);
+            t.true(skill.spellClasses[prof] > 0);
+        });
+
+        t.ok(skill.spellTargets);
+        t.ok(skill.spellDescription);
+        t.ok(skill.spellUseString);
+
+        t.true(Object.keys(skill.spellEffects).length > 0);
+
+        Object.keys(skill.spellEffects).forEach(skillEffect => {
+            if(skillEffect.chance) t.true(skillEffect.chance > 0 && skillEffect.chance <= 100);
+            if(skillEffect.roll) {
+                const testRoll = +Dice.roll(skillEffect.roll, player);
+                t.false(isNaN(testRoll));
+            }
+        });
+    });
+});
