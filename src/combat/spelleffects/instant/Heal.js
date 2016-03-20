659c97467dd729cfbf9d72ec9acbbacccc095ae8
seiyria <kyle@seiyria.com>
2016-03-20 11:02:38 -0500

    dead people can't be healed

--- src/combat/spelleffects/instant/Heal.js
+++ src/combat/spelleffects/instant/Heal.js
@@ -3,6 +3,7 @@ import SpellEffect from '../../base/SpellEffect';
 
 export default class Heal extends SpellEffect {
     apply(target) {
+        if(target.stats.hp.atMin()) return `${target.name} is currently dead and can't be healed!`;
         target.stats.hp.add(this.statBuff);
         return `${target.name} was healed for ${this.statBuff} HP!`;
     }
