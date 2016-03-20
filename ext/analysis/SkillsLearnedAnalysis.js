33f54349f7386d7f72a3a72892a2f5c841cec418
seiyria <kyle@seiyria.com>
2016-03-20 14:31:00 -0500

    adding first analysis tool

---
+++ ext/analysis/SkillsLearnedAnalysis.js
@@ -0,0 +1,32 @@
+
+import _ from 'lodash';
+
+import { allSkills } from '../../src/objects/skillmanager';
+const allProfessions = _.keys(require('require-dir')('../../src/character/professions'));
+
+import { longestString, startTravis, endTravis } from '../test/_helpers';
+
+startTravis('Skill Learning Analysis');
+
+_.each(allProfessions, profession => {
+    console.log(`${profession} Skills`);
+
+    const skillsLearned = _(allSkills)
+        .filter(skill => skill.spellClasses[profession])
+        .sortBy(skill => skill.spellClasses[profession])
+        .value();
+
+    if(!skillsLearned.length) {
+        return console.log('No skills for this class.\n');
+    }
+
+    const padSize = longestString(skillsLearned, 'spellName');
+
+    _.each(skillsLearned, skill => {
+        console.log(`${_.padRight(skill.spellName, padSize)}\tLevel ${_.padRight(skill.spellClasses[profession], 2)}\t${skill.spellDescription}`);
+    });
+
+    console.log();
+});
+
+endTravis('Skill Learning Analysis');
--- ext/test/_helpers.js
+++ ext/test/_helpers.js
@@ -1,6 +1,16 @@
 
+import _ from 'lodash';
 import { readFileSync } from 'fs';
 import path from 'path';
 import hjson from 'hjson';
 
 export const loadDataFile = (fileName) => hjson.parse(readFileSync(path.join(__dirname, '..', '..', 'data', `${fileName}.hjson`), 'utf8'));
+
+export const longestString = (arr, prop) => _.max(arr, skill => skill[prop].length)[prop].length;
+
+export const startTravis = (category) => {
+    console.log(`travis:start:${_.snakeCase(category)}`);
+    console.log(`${category}\n`);
+};
+
+export const endTravis   = (category) => console.log(`travis:end:${_.snakeCase(category)}`);
