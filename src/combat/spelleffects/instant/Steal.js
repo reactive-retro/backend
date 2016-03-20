50d742d5bd1b1308f8c4159f56cb57e831dedecd
seiyria <kyle@seiyria.com>
2016-03-20 11:11:10 -0500

    fixing a potentially bad issue with monsters stealing items

--- src/combat/spelleffects/instant/Steal.js
+++ src/combat/spelleffects/instant/Steal.js
@@ -18,6 +18,9 @@ export default class Steal extends SpellEffect {
                 return;
             }
 
+            // you should get this back after battle if a monster steals it from you
+            stolenItem.dropRate = 100;
+
             target.removeFromInventory(stolenItem);
             caster.addToInventory(stolenItem);
             messages.push(`${caster.name} stole ${stolenItem.name} from ${target.name}!`);
